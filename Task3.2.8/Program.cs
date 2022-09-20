b123e89620e63c484d57bbe61d65dba773e9a45e
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 18:50:03 +0300

    Добавлено задание 3.2.8

---
+++ Task3.2.8/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3_2_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Jane";
+
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0023");
+            Console.ReadKey();
+        }
+    }
+}
