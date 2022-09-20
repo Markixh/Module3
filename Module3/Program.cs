2d53b6a3cc4fe4394e682856b30ac157e8e1d069
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 19:43:10 +0300

    Добавлено финальное задание

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Jane";
-            Console.WriteLine(MyName);
-        }
+            Console.Write("Введите ваше Имя: ");
+            var name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            var age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Ваше имя {name}, а возраст {age}");
+            Console.Write("Введите дату своего рождения: ");
+            var birtdate = Console.ReadLine();
+            Console.Write("Ты родился {0}", birtdate);
+            Console.ReadKey();
+        }        
     }
 }
