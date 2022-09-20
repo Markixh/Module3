6bca198a46d5a8dea54e4b3951ba71c1d2cf6076
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 19:15:47 +0300

    Внесены изменения в задание 3.3.2

--- Task3.3.2/Program.cs
+++ Task3.3.2/Program.cs
@@ -5,13 +5,15 @@
         static void Main(string[] args)
         {
             const string Myname = "Андрей";
-            Console.WriteLine("Меня зовут {0}", Myname);
             const byte Age = 39;
-            Console.WriteLine("Мне {0} лет", Age);
             const Boolean Ifpit = false;
-            var Ip = Ifpit == true ? "да" : "нет";
-            Console.WriteLine("Есть ли у вас питомец? {0}", Ip);
             const float Fsize = 42;
+
+            var Ip = Ifpit == true ? "да" : "нет";
+
+            Console.WriteLine("Меня зовут {0}", Myname);            
+            Console.WriteLine("Мне {0} лет", Age);    
+            Console.WriteLine("Есть ли у вас питомец? {0}", Ip);            
             Console.WriteLine("Мой размер ноги {0}", Fsize);
             Console.ReadKey();
         }
