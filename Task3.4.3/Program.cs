55e5c4f5fbc249bbc7fe99a347af042755bafbd1
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 19:20:43 +0300

    Добавлено задание 3.4.3

---
+++ Task3.4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+    }
+}
