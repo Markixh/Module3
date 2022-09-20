7c6f4ec7de29f7b10d5fa1854d1e83a7d1d4bf3a
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 17:35:01 +0300

    Добавлено залание 3.2.6

---
+++ Task3.2.6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3_2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Jane";
+            Console.WriteLine(MyName);
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\tМне 27 лет");
+            Console.WriteLine("\tMy name is \n Jane");
+            Console.ReadLine();
+        }
+    }
+}
