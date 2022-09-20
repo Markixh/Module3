7c7872f963b8c4c79ca16ea45f7b95c40ffeea16
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 19:12:53 +0300

    Добавлено задание 3.3.2

---
+++ Task3.3.2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Myname = "Андрей";
+            Console.WriteLine("Меня зовут {0}", Myname);
+            const byte Age = 39;
+            Console.WriteLine("Мне {0} лет", Age);
+            const Boolean Ifpit = false;
+            var Ip = Ifpit == true ? "да" : "нет";
+            Console.WriteLine("Есть ли у вас питомец? {0}", Ip);
+            const float Fsize = 42;
+            Console.WriteLine("Мой размер ноги {0}", Fsize);
+            Console.ReadKey();
+        }
+    }
+}
