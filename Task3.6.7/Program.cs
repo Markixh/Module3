ba8f2aa31b52b6145a6658c3de983425a5ed5bc5
Марков Андрей <Andrejmarko@yandex.ru>
2022-09-20 19:29:14 +0300

    Добавлено задание 3.6.7

---
+++ Task3.6.7/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3_6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 10 % 3;
+            Console.WriteLine($"10 % 3 = {result}");
+            Console.ReadKey();
+        }
+    }
+}
